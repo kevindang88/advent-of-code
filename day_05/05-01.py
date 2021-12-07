38052e1e598b090e98009ecf93a8613f3bf1e91d
Kevin Dang <kevindang88@gmail.com>
2021-12-07 01:06:56 -0800

    Add day 5 part 1 solution

--- day_05/05-01.py
+++ day_05/05-01.py
@@ -1,8 +1,17 @@
 import re
 
 with open('day_05/input_05.txt') as input:
-    coords = []
-    overlaps = set()
+    coords = {}
     for line in input:
-        x1, y1, x2, y2 = re.split(',| -> ', line.strip())
-        
+        x1, y1, x2, y2 = [int(i) for i in re.split(',| -> ', line)]
+        if x1 == x2 or y1 == y2:
+            for i in range(abs(x1 - x2) + 1):
+                for j in range(abs(y1 - y2) + 1):
+                    c = (min(x1, x2) + i, min(y1, y2) + j)
+                    if c not in coords:
+                        coords[c] = 1
+                    else:
+                        coords[c] += 1
+
+    overlaps = sum(x > 1 for x in coords.values())
+    print(overlaps)
