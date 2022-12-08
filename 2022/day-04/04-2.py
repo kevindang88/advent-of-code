533c186ca293f6cc055bf6ab89f8b7bdea0afc9c
Kevin Dang <kevindang88@gmail.com>
2022-12-07 22:45:51 -0800

    Format code

--- 2022/day-04/04-2.py
+++ 2022/day-04/04-2.py
@@ -9,7 +9,7 @@ with open('input.txt') as input:
             break
         sections = list(map(lambda s: int(s), re.split(',|-', line)))
         if ((sections[0] <= sections[2] and sections[1] >= sections[2])
-            or (sections[2] <= sections[0] and sections[3] >= sections[0])):
+                or (sections[2] <= sections[0] and sections[3] >= sections[0])):
             overlaps += 1
 
 print(overlaps)
