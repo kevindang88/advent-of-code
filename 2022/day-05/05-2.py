f4aaa82799b7955a42f6dee2d6e83f6c63268cfb
Kevin Dang <kevindang88@gmail.com>
2022-12-08 17:03:55 -0800

    Add day 5, part 2

---
+++ 2022/day-05/05-2.py
@@ -0,0 +1,41 @@
+import math
+import re
+
+stacks = [ [] for _ in range(9) ]
+
+with open('input.txt') as input:
+    # read crates
+    while True:
+        line = input.readline()
+        if line[0].isspace():
+            break
+
+        # read characters
+        line_length = len(line)
+        for i in range(line_length):
+            if line[i].isalpha():
+                target_stack = math.ceil(i / 4) - 1
+                stacks[target_stack].insert(0, line[i]) # insert in front
+
+    # consume empty line
+    input.readline()
+
+    # read moves
+    while True:
+        line = input.readline()
+        if line.isspace() or not line:
+            break
+        move = re.split(' from | to ', line[5:].strip())
+        if len(move) <= 1:
+            break
+        crates_to_move = int(move[0])
+        from_stack = int(move[1]) - 1
+        to_stack = int(move[2]) - 1
+        from_stack_size = len(stacks[from_stack])
+        pop_index = from_stack_size - crates_to_move
+        for crates in range(crates_to_move):
+            stacks[to_stack].append(stacks[from_stack].pop(pop_index))
+
+    # get top crates
+    top_crates = ''.join([ s.pop() for s in stacks ])
+    print(top_crates)
