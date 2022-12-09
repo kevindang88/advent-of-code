3d13069b6c2783e984340d27aace7341bcb47d6a
Kevin Dang <kevindang88@gmail.com>
2022-12-08 21:18:12 -0800

    Create anonymous set

--- 2022/day-06/06-2.py
+++ 2022/day-06/06-2.py
@@ -3,18 +3,16 @@ import collections
 MARKER_LENGTH = 14
 
 buffer = collections.deque([], maxlen = MARKER_LENGTH)
-buffer_set = set()
 
 with open('input.txt') as file:
     input = file.readline()
     for i in range(MARKER_LENGTH):
         buffer.append(input[i])
     for i in range(len(input)):
-        buffer_set.update(buffer)
-        if len(buffer_set) == MARKER_LENGTH:
+        if len(set(buffer)) == MARKER_LENGTH:
             marker = i
             break
-        buffer_set.remove(buffer.popleft())
+        buffer.popleft()
         buffer.append(input[i])
 
 print(marker)
