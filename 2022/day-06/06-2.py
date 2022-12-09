033e74969b60aae22febfd663957934d3163d32a
Kevin Dang <kevindang88@gmail.com>
2022-12-08 21:11:07 -0800

    Add day 6,  part 2

--- 2022/day-06/06-2.py
+++ 2022/day-06/06-2.py
@@ -1,15 +1,15 @@
 import collections
 
-buffer = collections.deque([], maxlen = 4)
+buffer = collections.deque([], maxlen = 14)
 buffer_set = set()
 
 with open('input.txt') as file:
     input = file.readline()
-    for i in range(4):
+    for i in range(14):
         buffer.append(input[i])
     for i in range(len(input)):
         buffer_set.update(buffer)
-        if len(buffer_set) == 4:
+        if len(buffer_set) == 14:
             marker = i
             break
         buffer_set.remove(buffer.popleft())
